1b31ef24a943b8c8acad09effed09f15d1d607a9
Simon Smida <simon.smida@gmail.com>
2021-04-10 19:31:12 +0200

    Adding documentation to standardDeviation.py

--- src/standardDeviation.py
+++ src/standardDeviation.py
@@ -3,7 +3,13 @@
 from MathLib import *
 import sys
 import re
+from statistics import stdev
 
+## 
+# @brief    Calculate variance from given data
+# 
+# @param    values  list of numbers separated with whitespace
+# @return   variance of numbers
 def variance(values):
     N = len(values)
     s_mean = 0
@@ -14,6 +20,11 @@ def variance(values):
     mean = Divide(s_mean, N)
     return Divide(Sub(s_seq, Multiply(N, Power(mean, 2))), N-1)
 
+##
+# @brief    Calculate standard deviation from given data
+#
+# @param    values  list of numbers separated with whitespace
+# @return   standard deviation of numbers
 def standardDeviation(values):
     return Root(variance(values), 2)
 
@@ -25,7 +36,9 @@ for line in sys.stdin:
     # supported separation: (, ) - comma+space
     #                       (,) - comma only
     #                       ( ) - space only
-    values += [int(x) for x in re.split(', |,| ', data)]
+    values += [float(x) for x in re.split(', |,| ', data)]
 
+#print(values)
 # Example usage: ./standardDeviation.py < data.txt
 print(standardDeviation(values))
+print(stdev(values))
