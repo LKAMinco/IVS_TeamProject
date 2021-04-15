e79001038186868fb1fdb0bd02ef0b45be901c8d
fbucko <79278844+fbucko@users.noreply.github.com>
2021-04-15 20:31:15 +0200

    Deleted main at the end of file and replaced to another file

--- src/gui.py
+++ src/gui.py
@@ -449,11 +449,3 @@ class Ui_MainWindow(object):
         self.actionfsdfs.setText(_translate("MainWindow", "fsdfs"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
