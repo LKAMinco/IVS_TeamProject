3892f831a5826b8d283011d1d0c7dace8ac521b7
fbucko <79278844+fbucko@users.noreply.github.com>
2021-04-16 19:36:23 +0200

    Added links for advanced math operations

--- src/CalcInterface.py
+++ src/CalcInterface.py
@@ -30,17 +30,28 @@ class CalculatorWindow(QtWidgets.QMainWindow,Ui_MainWindow):
 		self.pushButton_six.clicked.connect(self.digit_pressed)
 		self.pushButton_eight.clicked.connect(self.digit_pressed)
 		self.pushButton_nine.clicked.connect(self.digit_pressed)
+	
 		#Create button connections for decimal point
 		self.pushButton_dot.clicked.connect(self.decimal_pressed)
+		
 		#Create button connections for clearing display
 		self.pushButton_clear.clicked.connect(self.clear_pressed)	
 		self.pushButton_del.clicked.connect(self.del_pressed)
+		
 		#Create button connections for math operations
 		self.pushButton_add.clicked.connect(self.add_pressed)
 		self.pushButton_sub.clicked.connect(self.sub_pressed)
 		self.pushButton_mul.clicked.connect(self.mul_pressed)
 		self.pushButton_div.clicked.connect(self.div_pressed)	
+		self.pushButton_pow.clicked.connect(self.pow_pressed)	
+		self.pushButton_sqr.clicked.connect(self.sqr_pressed)	
 		self.pushButton_result.clicked.connect(self.result_pressed)
+	
+		#Create button connections for advanced math operations	
+		self.action_sin.triggered.connect(self.sin_pressed)
+		self.action_cos.triggered.connect(self.cos_pressed)
+		self.action_abs.triggered.connect(self.abs_pressed)
+		self.action_fac.triggered.connect(self.fac_pressed)
 
 	def digit_pressed(self):
 		button = self.sender()
@@ -84,15 +95,41 @@ class CalculatorWindow(QtWidgets.QMainWindow,Ui_MainWindow):
 		newLabel = self.lineEdit_results.text() + button.text()
 		self.lineEdit_results.setText(newLabel)
 
+	def pow_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + button.text()
+		self.lineEdit_results.setText(newLabel)
+	
+	def sqr_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + button.text()
+		self.lineEdit_results.setText(newLabel)
+	
 	def result_pressed(self):
 		result = self.lineEdit_results.text()
 		result = InputProcessing(result)
 		self.lineEdit_results.setText(str(result))
 
+	def sin_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + 'sin'
+		self.lineEdit_results.setText(newLabel)
+	def cos_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + 'cos' 
+		self.lineEdit_results.setText(newLabel)
+	def abs_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + 'abs'
+		self.lineEdit_results.setText(newLabel)
+	def fac_pressed(self):
+		button = self.sender()
+		newLabel = self.lineEdit_results.text() + '!'
+		self.lineEdit_results.setText(newLabel)
 ##
 # @brief    Processing of user input
 #
-# @param    a    input in string format
+# @param    a input in string format
 #
 # @return   Calculates the result of inserted numbers,operands
 
@@ -109,59 +146,59 @@ def InputProcessing(input):
 	else:
 		number = float(sInput[0])
 		for i in range(0, len(sInput)):
-		if sInput[i] == '+':
-			#Checks if next number is positiv or negative, in case of negative number it modifies the list
-			if sInput[i + 1] == '':
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0' #Replacement for '-', wont affect calculations
-			number = MathLib.Add(float(number), float(sInput[i + 1]))
-		elif sInput[i] == '-':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Sub(float(number), float(sInput[i + 1]))
-		elif sInput[i] == '*':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Multiply(float(number), float(sInput[i + 1]))
-		elif sInput[i] == '/':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			#Division by 0 handling
-			if sInput[i + 1] == '0' or sInput[i + 1] == -0:
-				return 'Error'
-			number = MathLib.Divide(float(number), float(sInput[i + 1]))
-		elif sInput[i] == '!':
-			# Checks if number is positive or negative
-			if number >= 0:
-				number = MathLib.Fac(float(number))
-			else:
-				number = -1 * MathLib.Fac(-1*float(number))
-		elif sInput[i] == 'abs':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Abs(float(sInput[i + 1]))
-		elif sInput[i] == 'sin':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Sin(float(sInput[i + 1]))
-		elif sInput[i] == 'cos':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1)*float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Cos(float(sInput[i + 1]))
-		elif sInput[i] == '^':
-			if sInput[i + 1] == '': #Look at line 113 for definition
-				sInput[i + 1] = int(-1) * float(sInput[i + 3])
-				sInput[i + 2] = '0'
-			number = MathLib.Power(float(number), float(sInput[i + 1]))
-		elif sInput[i] == '√':
-			#If number is negative it returns error
-			if sInput[i + 1] == '':
-				return 'Error'
-			number = MathLib.Root(float(sInput[i + 1]),float(sInput[i - 1]))
+			if sInput[i] == '+':
+				#Checks if next number is positiv or negative, in case of negative number it modifies the list
+				if sInput[i + 1] == '':
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0' #Replacement for '-', wont affect calculations
+				number = MathLib.Add(float(number), float(sInput[i + 1]))
+			elif sInput[i] == '-':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Sub(float(number), float(sInput[i + 1]))
+			elif sInput[i] == '*':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Multiply(float(number), float(sInput[i + 1]))
+			elif sInput[i] == '/':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				#Division by 0 handling
+				if sInput[i + 1] == '0' or sInput[i + 1] == -0:
+					return 'Error'
+				number = MathLib.Divide(float(number), float(sInput[i + 1]))
+			elif sInput[i] == '!':
+				# Checks if number is positive or negative
+				if number >= 0:
+					number = MathLib.Fac(float(number))
+				else:
+					number = -1 * MathLib.Fac(-1*float(number))
+			elif sInput[i] == 'abs':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Abs(float(sInput[i + 1]))
+			elif sInput[i] == 'sin':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Sin(float(sInput[i + 1]))
+			elif sInput[i] == 'cos':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1)*float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Cos(float(sInput[i + 1]))
+			elif sInput[i] == '^':
+				if sInput[i + 1] == '': #Look at line 113 for definition
+					sInput[i + 1] = int(-1) * float(sInput[i + 3])
+					sInput[i + 2] = '0'
+				number = MathLib.Power(float(number), float(sInput[i + 1]))
+			elif sInput[i] == '√':
+				#If number is negative it returns error
+				if sInput[i + 1] == '':
+					return 'Error'
+				number = MathLib.Root(float(sInput[i + 1]),float(sInput[i - 1]))
 	return number
